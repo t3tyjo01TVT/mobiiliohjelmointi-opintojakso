b37db3675a732645469a2042669d975efd48620a
Johannes Typpö <t3tyjo01@students.oamk.fi>
2025-10-30 20:56:54 +0200

    currencycalculator

--- Exercises/CurrencyCalculator/App.tsx
+++ Exercises/CurrencyCalculator/App.tsx
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [euros, setEuros] = useState<string>("");
+  const eurAsNumber: number =
+    !isNaN(Number(euros)) === true ? Number(euros) : 0;
+  const pounds: number = eurAsNumber * 0.9;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>Currency calculator</Text>
+      <TextInput
+        placeholder="€"
+        keyboardType="number-pad"
+        value={euros}
+        onChangeText={setEuros}
+      />
+      <Text style={styles.text}>{pounds.toFixed(2)} £</Text>
     </View>
   );
 }
@@ -13,8 +24,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 16,
+  },
+  heading: {
+    fontSize: 24,
+    marginTop: 32,
+    marginBottom: 16,
+  },
+  text: {
+    marginVertical: 16,
   },
 });
